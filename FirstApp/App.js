48b273b6531be7624f4695cf08b9c2410dfd3c8b
Hamou Maamar Sabah <saabaaah@gmail.com>
2020-03-23 17:10:59 +0100

    first commit

---
+++ FirstApp/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World! by sabah.HM</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
